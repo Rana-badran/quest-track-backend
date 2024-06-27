9ae3509ea0d6a0b039b37586749b70aaf30d8f76
Rana Badran <ranaz@Ranas-MacBook-Pro.local>
2024-06-27 12:50:51 -0400

    database set up with more file set up

--- routes/quests.js
+++ routes/quests.js
@@ -1,1 +1,17 @@
+ const router = require("express").router();
+
  
+//Users:
+
+// POST /auth logging in
+// POST /auth/register  registering
+// PUT /auth/edit updating password/email
+ 
+// Tasks:
+//  GET /tasks gets all tasks
+//  POST/ Quest
+//  PUT/ Quest/:id
+//  Delete/ Quest/:id
+
+
+ module.exports = router;
